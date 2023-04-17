018ad479f9cc29eee4b8edb718479890bd1abf4a
Daniel Galdino <danoligal@gmail.com>
2023-04-17 16:41:07 -0300

    Implementa validações de formulário

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,39 +1,51 @@
 import { useForm } from "react-hook-form";
 import styles from "./style.module.css"
+import { Input } from "../../components/input";
+import { FormGroup } from "../../components/FormGroup";
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup"
+import { Link } from "react-router-dom";
 
 interface LoginData {
     email:string;
     senha: string;
 }
 
+const schema = yup.object().shape({
+    email: yup
+    .string().typeError("Os dados envidos são inválidos")
+    .email("O email informado não e válido")
+    .required("Informe seu email"),
+    senha: yup
+    .string().typeError("Os dados envidos são inválidos")
+    .min(8,"Sua senha deve conter ao memos 8 caracteres")
+    .required("Informe sua senha")
+});
+
 const Login = () => {
-const {register, handleSubmit, formState:{errors}} = useForm<LoginData>();
+const {register, handleSubmit, formState:{errors}} = useForm<LoginData>({resolver:yupResolver(schema)});
 
 const onSubmit =(dados:LoginData) =>{
     console.log(dados)
 }
 
-    return (<div className={styles.container} >
+    return (
+    <div className={styles.container} >
         <form onSubmit={handleSubmit(onSubmit)}>
         <main className={styles.main}>
         <h1 className={styles.title}>Faça Login</h1>
-        <div className={styles.formGroup}>
-            <label className={styles.label}>Email</label>
-            <input className={styles.input} type="email" id="email" {...register("email",{ required:true})}/>
-            {errors?.email && errors.email.message}
-        </div>
-        <div className={styles.formGroup}>
-            <label className={styles.label} htmlFor="senha">Senha</label>
-            <input className={styles.input} type="password" id="senha"{...register("senha",{minLength:8, required:true})} />
-            {errors?.email && errors.email.message}
-        </div>
-        <div className={styles.formGroup}>
+        <Input label="Email" id="email" type="email" {...register("email")} error={errors?.email?.message} />
+        <Input label="Senha" id="senha" type="password" {...register("senha")} error={errors?.senha?.message} />
+        <FormGroup>
             <button className={styles.login}>Logar</button>
-        </div>
-        
+        </FormGroup>  
+        <div className={styles.mainFooter}>
+            <p>Ainda não possue conta? <Link to="/registro"> Cadastre-se</Link></p>
+        </div>      
     </main>
     </form>
-    </div>)
-}
+    </div>
+    );
+};
 
 export default Login;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom"
 import Login from "./pages/Login"
+import Registro from "./pages/Registro"
 
 export const routes = createBrowserRouter([
     {
         path: "/login",
         element: <Login/>
+    },
+    {
+        path: "/registro",
+        element: <Registro/>
     }
 ]) 
     
